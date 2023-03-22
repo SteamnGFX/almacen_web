637a6fa5a5b3d9a039d954844344b2fd23696007
DamianGL3 <damiangamboa0310@gmail.com>
2023-03-22 16:37:26 -0600

    Color Almacenes modificado

--- web/modulos/moduloAlmacen/controlador_Almacen.js
+++ web/modulos/moduloAlmacen/controlador_Almacen.js
@@ -1,5 +1,3 @@
-
-
 var ctx1 = document.getElementById('myChart1').getContext('2d');
 var ctx2 = document.getElementById('myChart2').getContext('2d');
 var ctx3 = document.getElementById('myChart3').getContext('2d');
@@ -14,16 +12,27 @@ var padreG4 = document.getElementById('padreG4');
 var padreG5 = document.getElementById('padreG5');
 var padreG6 = document.getElementById('padreG6');
 
-var data = {        
+var chart1;
+var chart2;
+var chart3;
+var chart4;
+var chart5;
+var chart6;
+
+function cambiarTitulo(){
+    document.title = "Almacen";
+}
+
+function cargarGraficas() {
+    var data = {
         datasets: [{
                 label: 'Existencias',
                 data: [20, 80],
                 backgroundColor: ['#27368B', '#FFFFFF']
             }]
     };
-    
-    
-var options = {
+
+    var options = {
         tooltips: {
             enabled: true
         },
@@ -48,66 +57,66 @@ var options = {
         animation: {
             animateScale: true
         },
-        
+
         hover: {
             mode: null
         }
     };
-    
-    
-var chart1 = new Chart('myChart1', {
-      type: 'doughnut',
-      data: data,
-      options: options
+
+    chart1 = new Chart('myChart1', {
+        type: 'doughnut',
+        data: data,
+        options: options
     });
-    
-var chart2 = new Chart('myChart2', {
-      type: 'doughnut',
-      data: data,
-      options: options
+
+    chart2 = new Chart('myChart2', {
+        type: 'doughnut',
+        data: data,
+        options: options
     });
-    
-var chart3 = new Chart('myChart3', {
-      type: 'doughnut',
-      data: data,
-      options: options
+
+    chart3 = new Chart('myChart3', {
+        type: 'doughnut',
+        data: data,
+        options: options
     });
-    
-var chart4 = new Chart('myChart4', {
-      type: 'doughnut',
-      data: data,
-      options: options
+
+    chart4 = new Chart('myChart4', {
+        type: 'doughnut',
+        data: data,
+        options: options
     });
-    
-var chart5 = new Chart('myChart5', {
-      type: 'doughnut',
-      data: data,
-      options: options
+
+    chart5 = new Chart('myChart5', {
+        type: 'doughnut',
+        data: data,
+        options: options
     });
-    
-var chart6 = new Chart('myChart6', {
-      type: 'doughnut',
-      data: data,
-      options: options
+
+    chart6 = new Chart('myChart6', {
+        type: 'doughnut',
+        data: data,
+        options: options
     });
-   
+
+}
 
 function updateStatus() {
     var percentage = chart1.data.datasets[0].data[0];
-    
+
     if (percentage >= 100) {
 
-        padreG1.style.backgroundColor = '#F44336';
+        padreG1.style.backgroundColor = '#FFFFFF';
     } else if (percentage <= 0) {
 
         padreG1.style.backgroundColor = '#4CAF50';
     } else {
 
-        padreG1.style.backgroundColor = '#4CAF50';
+        padreG1.style.backgroundColor = '#09EF6B';
     }
-    
+
     var percentage = chart2.data.datasets[0].data[0];
-    
+
     if (percentage >= 100) {
 
         padreG2.style.backgroundColor = '#F44336';
@@ -118,9 +127,9 @@ function updateStatus() {
 
         padreG2.style.backgroundColor = '#4CAF50';
     }
-    
+
     var percentage = chart3.data.datasets[0].data[0];
-    
+
     if (percentage >= 100) {
 
         padreG3.style.backgroundColor = '#F44336';
@@ -131,9 +140,9 @@ function updateStatus() {
 
         padreG3.style.backgroundColor = '#4CAF50';
     }
-    
+
     var percentage = chart4.data.datasets[0].data[0];
-    
+
     if (percentage >= 100) {
 
         padreG4.style.backgroundColor = '#F44336';
@@ -144,9 +153,9 @@ function updateStatus() {
 
         padreG4.style.backgroundColor = '#4CAF50';
     }
-    
+
     var percentage = chart5.data.datasets[0].data[0];
-    
+
     if (percentage >= 100) {
 
         padreG5.style.backgroundColor = '#F44336';
@@ -157,9 +166,9 @@ function updateStatus() {
 
         padreG5.style.backgroundColor = '#4CAF50';
     }
-    
+
     var percentage = chart6.data.datasets[0].data[0];
-    
+
     if (percentage >= 100) {
 
         padreG6.style.backgroundColor = '#F44336';
@@ -170,7 +179,9 @@ function updateStatus() {
 
         padreG6.style.backgroundColor = '#4CAF50';
     }
-    
+
 }
 
+cargarGraficas();
 updateStatus();
+cambiarTitulo();
